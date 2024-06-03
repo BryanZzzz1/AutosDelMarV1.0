0a4b0ee062ec05e974784760eb67fb810b1343f0
Linguiniprogramacion <163914530+Linguiniprogramacion@users.noreply.github.com>
2024-06-03 15:55:58 -0400

    Update scriptsesion.js
    user : admin
    contraseña : 123

--- js/scriptsesion.js
+++ js/scriptsesion.js
@@ -25,8 +25,8 @@ pwShowHide.forEach((icon) => {
 document.getElementById('loginForm').addEventListener('submit', function(event) {
   event.preventDefault();
   
-  let username = document.getElementById('username').value;
-  let password = document.getElementById('password').value;
+  let username = document.getElementById('admin').value;
+  let password = document.getElementById('123').value;
   
   if (username === 'usuario' && password === 'contraseña') {
     localStorage.setItem('loggedIn', true);
